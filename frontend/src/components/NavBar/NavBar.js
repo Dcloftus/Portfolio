1b3b83a975bc7b873e191ce8ba5eee56d66cc9cf
Daniel Loftus <dcloftus@hotmail.com>
2024-09-23 15:45:46 -0400

    Working on the nav bar being animated

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import { ReactComponent as UpArrow } from '../../assets/icons/UpArrow.svg';
+
 import './NavBar.css';
 
 function NavBar() {
+    const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+    useEffect(() => {
+        console.log(window.scrollY)
+        const handleScroll = () => {
+            if (window.scrollY > 50) { // Show button after scrolling down 300px
+                setShowScrollToTop(true);
+            } else {
+                setShowScrollToTop(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="navbar m-plus-rounded-1c-black">
+        <ul className={`nav-items ${showScrollToTop ? 'shift-left' : ''}`}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/about">About Me</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className='scrollToTopContainer'>
+            {showScrollToTop && (
+                <button className={`scrollToTop ${showScrollToTop ? 'show' : ''}`} onClick={scrollToTop}>
+                    <UpArrow />
+                </button>
+            )}
+        </div>
     </nav>
   );
 }
