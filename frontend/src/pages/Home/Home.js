c1f4b7ff1d59916de1388d30515da42db91c1edb
Daniel Loftus <dcloftus@hotmail.com>
2024-09-24 15:28:30 -0400

    Hero and structure done for now

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './Home.css';
 
+import PillLabels from '../../components/PillLabels/PillLabels.js'
+
 // Images
 import hero from '../../assets/images/DanielHerox2.png'
-import PillLabels from '../../components/PillLabels/PillLabels';
+
+import { ReactComponent as StarLg } from '../../assets/icons/StarLg.svg';
+import { ReactComponent as StarMd } from '../../assets/icons/StarMd.svg';
+import { ReactComponent as StarSm } from '../../assets/icons/StarSm.svg';
+import { ReactComponent as TadpoleLg } from '../../assets/icons/TadpoleLg.svg';
+import { ReactComponent as TadpoleMd } from '../../assets/icons/TadpoleMd.svg';
+import { ReactComponent as TadpoleSm } from '../../assets/icons/TadpoleSm.svg';
 
 function Home() {
     return (
         <div>
             <div className='heroContainer'>
                 <div className='wrapper'>
+                    <div className='StarsContainer'>
+                        <StarLg className='StarLg' />
+                        <StarSm className='StarSm' />
+                        <StarMd className='StarMd' />
+                    </div>
                     <div className='heroContentContainer'>
                         <div className='heroTextContainer'>
                             <h4>Hey there! 👋</h4>
@@ -21,6 +34,11 @@ function Home() {
                         <div className='heroBackground'>
                             <button className='UIFlowButton'/>
                         </div>
+                    </div>
+                    <div className='TadpoleContainer'>
+                        <TadpoleLg className='TadpoleLg' />
+                        <TadpoleMd className='TadpoleMd' />
+                        <TadpoleSm className='TadpoleSm' />
                     </div>
                 </div>
                 <img src={hero} alt='Daniel' className='hero' />
