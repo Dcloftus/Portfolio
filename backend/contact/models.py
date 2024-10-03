016ef7a6afe8ae5d5b07ba33e99e9029d56bacdf
Daniel Loftus <dcloftus@hotmail.com>
2024-10-03 08:54:06 -0400

    Contact sending to DB done!

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    email = models.EmailField(max_length=254)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254, unique=True)
     message = models.TextField()
+    sent_date_utc = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
