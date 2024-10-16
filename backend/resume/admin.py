ece914a589e28b139a7aa6805400a9cf12032473
Daniel Loftus <dcloftus@hotmail.com>
2024-10-16 14:20:22 -0400

    Resume APIs set up

--- backend/resume/admin.py
+++ backend/resume/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Profile, Company, Experience, JobDescription, Education, Skills
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Company)
+admin.site.register(Experience)
+admin.site.register(JobDescription)
+admin.site.register(Education)
+admin.site.register(Skills)
--- backend/resume/views.py
+++ backend/resume/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProfileSerializer, CompanySerializer, ExperienceSerializer, JobDescriptionSerializer, EducationSerializer, SkillsSerializer
+from django.db import IntegrityError
 
-# Create your views here.
+from django.http import JsonResponse
+
+from .models import Profile, Company, Experience, JobDescription, Education, Skills
+
+# API Needs
+# Get Profile All Entries
+@api_view(['GET'])
+def getProfile(request):
+    profile = Profile.objects.all()
+    serializer = ProfileSerializer(profile, many=True)
+    return Response(serializer.data)
+
+# GET Work Experience All positions with company info and descriptions
+@api_view(['GET'])
+def getWorkExperience(request):
+    experiences = Experience.objects.all()
+    serializer = ExperienceSerializer(experiences, many=True)
+    return Response(serializer.data)
+
+# GET all Education Entries
+@api_view(['GET'])
+def getEducation(request):
+    education = Education.objects.all()
+    serializer = EducationSerializer(education, many=True)
+    return Response(serializer.data)
+
+# GET all Skills
+@api_view(['GET'])
+def getSkills(request):
+    skills = Skills.objects.all()
+    serializer = SkillsSerializer(skills, many=True)
+    return Response(serializer.data)
