da89d15e1e36f7004a4732bb6ed25015ea57848b
Daniel Loftus <dcloftus@hotmail.com>
2024-11-22 15:51:41 -0500

    Updating the background image to cover the project card

--- frontend/src/components/CardTypes/ProjectCard.js
+++ frontend/src/components/CardTypes/ProjectCard.js
@@ -6,7 +6,7 @@ import './ProjectCard.css';
 const ProjectCard = ({ title, description, backgroundImage, link }) => {
     console.log(backgroundImage);
     return (
-        <div className='projectCard' style={{backgroundImage:`url(${backgroundImage})`}}>
+        <div className='projectCard' style={{backgroundImage:`url(${backgroundImage})`, backgroundSize:'cover'}}>
             <div className='cardContents'>
                 <h2>{title}</h2>
                 <p>{description}</p>
