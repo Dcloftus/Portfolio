72e62c7464366901171e48a996110b7f406d395c
Daniel Loftus <dcloftus@hotmail.com>
2025-01-03 15:08:46 -0500

    Skeleton loading for all projects

--- frontend/src/pages/Projects/Projects.js
+++ frontend/src/pages/Projects/Projects.js
@@ -6,14 +6,19 @@ import ProjectCard from '../../components/CardTypes/ProjectCard.js';
 
 function Projects() {
     const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     function getProjects() {
         fetch(`${process.env.REACT_APP_API_BASE_URL}/projects`)
         .then((response) => response.json())
         .then((data) => {
             setProjects(data);
+            setLoading(false); // Set loading to false once data is fetched
         })
-        .catch((error) => console.error('Error fetching Projects:', error));
+        .catch((error) => {
+            console.error('Error fetching Projects:', error);
+            setLoading(false); // Set loading to false even if there's an error
+        });
     }
 
     useEffect(() => {
@@ -27,11 +32,24 @@ function Projects() {
                     <h1>Projects</h1>
                 </div>
             </div>
-            <Carousel items={
-                projects.map(project => (
-                    <ProjectCard key={project.id} title={project.name} description={project.description} backgroundImage={project.cover} link="/loftusco"/>
-                ))
-            } />
+            {loading ? ( // Show loading animation if still loading
+                <div className='loadingWrapper'>
+                    <div className="loadingContainer">
+                        <div className='loadingCard'/>
+                        <div className='loadingCard'/>
+                        <div className='loadingCard'/>
+                        <div className='loadingCard'/>
+                        <div className='loadingCard'/>
+                        <div className='loadingCard'/>
+                    </div>
+                </div>
+            ) : (
+                <Carousel items={
+                    projects.map(project => (
+                        <ProjectCard key={project.id} title={project.name} description={project.description} backgroundImage={project.cover} link="/loftusco"/>
+                    ))
+                } />
+            )}
         </div>
     );
 }
